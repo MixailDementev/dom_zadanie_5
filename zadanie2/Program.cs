aadd100e83e1ab75b4a19812edb747d496608a1c
MDementev <vanneste1207@gmail.com>
2022-09-04 14:06:14 +0300

    1 задача

--- zadanie2/Program.cs
+++ zadanie2/Program.cs
@@ -1,6 +1,7 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
-// Напишите программу, которая покажет количество чётных чисел в массиве.
-// [345, 897, 568, 234] -> 2
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
 
 int[] EnterArray()
 {
@@ -10,15 +11,15 @@ int[] EnterArray()
     int[] arr = new int[number];
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(99, 1000);
+        arr[i] = new Random().Next(0, 100);
     }
     Console.WriteLine($"You Array: {String.Join(", ", arr)}");
     return arr;
 }
 
-void NumberOfEvenElements(int[] arr)
+void SumOfOddElements(int[] arr)
 {
-    int count = 0;
+    int sum = 0;
     for (int i = 0; i < arr.Length; i++)
     {
         if (arr[i] % 2 == 0)
@@ -29,3 +30,4 @@ void NumberOfEvenElements(int[] arr)
     Console.WriteLine($"Count even elements:\t" + count);
 }
 NumberOfEvenElements(EnterArray());
+
